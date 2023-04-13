c56f8a5454eda647ab36556b18da83247e8b176f
Jean5027 <112779029+Jean5027@users.noreply.github.com>
2023-04-13 08:47:50 -0300

    outro commit
    taaaaaaaaaa

--- mercado/models.py
+++ mercado/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 
-class Airport(models.Model):
+class Mercado(models.Model):
     code = models.CharField(max_length=3)
     city = models.CharField(max_length=64)
--- mercado/urls.py
+++ mercado/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from . import views
 
 
 urlpatterns = [
-    
-
+    path('', views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
 ]
